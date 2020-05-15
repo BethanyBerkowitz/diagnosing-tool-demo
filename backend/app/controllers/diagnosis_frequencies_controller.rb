fc002a6b066f4af25e9e9e8d470576ecc45d4b55
Bethany Berkowitz <bethany@Hildegarde.local>
2020-05-15 15:01:59 -0400

    Add controllers and routes

---
+++ backend/app/controllers/diagnosis_frequencies_controller.rb
@@ -0,0 +1,16 @@
+class DiagnosisFrequenciesController < ApplicationController
+    def index
+        @frequencies = DiagnosisFrequency.all
+        render :json => @frequencies
+    end 
+
+    def show
+        @frequency = DiagnosisFrequency.find(params[:id])
+        render :json => @frequency
+    end
+
+    def create
+        DiagnosisFrequency.add(diagnosis_id: params[:diagnosis_id], symptom_id: params[symptom_id])
+        render :created
+    end
+end
