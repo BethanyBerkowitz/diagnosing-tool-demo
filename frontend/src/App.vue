ebbdc37c313fcde1f2a7059b46ec315ec9368144
Bethany Berkowitz <bethany@Hildegarde.local>
2020-05-15 16:36:38 -0400

    Add symptom picker

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SymptomPicker/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SymptomPicker from './components/SymptomPicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SymptomPicker
   }
 }
 </script>
