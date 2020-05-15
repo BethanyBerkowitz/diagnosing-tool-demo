61af62a05077176c40a7b7e64f4277e309d04f87
Bethany Berkowitz <bethany@Hildegarde.local>
2020-05-15 15:01:59 -0400

    Add controllers and routes

--- backend/app/models/diagnosis_frequency.rb
+++ backend/app/models/diagnosis_frequency.rb
@@ -1,4 +1,13 @@
 class DiagnosisFrequency < ApplicationRecord
     belongs_to :diagnosis 
     belongs_to :symptom
+
+    def self.add(diagnosis_id:, symptom_id:)
+        match = self.find_by(diagnosis_id: diagnosis_id, symptom_id: symptom_id)
+        if match
+            match.increment!(:frequency)
+        else
+            self.create!(diagnosis_id: diagnosis_id, symptom_id: symptom_id, frequency: 1)
+        end
+    end
 end
